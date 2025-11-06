6fa2205f99ff7bf35baf55f51ee6b0f088d53136
saucymeatball123 <paniaguajuanjr24@gmail.com>
2025-11-05 20:27:12 -0500

    Put whatever you did here

---
+++ EmployeeInfo.h
@@ -0,0 +1,38 @@
+//
+#ifndef PROTOTYPE1_EMPLOYEEINFO_H
+#define PROTOTYPE1_EMPLOYEEINFO_H
+
+#include <string>
+
+class Person {
+public:
+    std::string name;
+    int age;
+
+    void inputPersonInfo();
+    void displayPersonInfo() const;
+};
+
+class Employee : public Person {
+public:
+    long double salary;
+    long double hours;
+    std::string employeeID;
+
+    void inputEmployeeInfo();
+    void displayEmployeeInfo() const;
+};
+
+class ConfirmInfo : public Employee {
+public:
+    void confirmInfo();
+};
+
+#endif // PROTOTYPE1_EMPLOYEEINFO_H
+// Created by JUAND on 11/5/2025.
+//
+
+#ifndef TASKGRID_EMPLOYEEINFO_H
+#define TASKGRID_EMPLOYEEINFO_H
+
+#endif //TASKGRID_EMPLOYEEINFO_H
--- main.cpp
+++ main.cpp
@@ -1,3 +1,16 @@
 //Juan Task
 // Manuel Task
-//
+# include <iostream>
+#include "EmployeeInfo.h"
+using namespace std;
+
+int main() {
+    ConfirmInfo e1;
+    e1.inputEmployeeInfo();
+
+    cout << "\nEmployee Information:\n";
+    e1.displayEmployeeInfo();
+    e1.confirmInfo();
+
+    return 0;
+}
