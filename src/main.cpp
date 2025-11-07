3ec0a7a85b1211c76faf7ff6c5bad6e5ffa00d62
mrosadevs <giftedlean@gmail.com>
2025-11-07 12:49:55 -0500

    Cleaned Cmakelists added QT QML Integration

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,13 @@
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
-#include <QQmlContext>
+#include <iostream>
+#include "EmployeeInfo.h"
+using namespace std;
 
-#include "app/App.hpp"
-#include "models/TaskModel.hpp"
+int main()
+{
+    ConfirmInfo employee;
+    employee.inputEmployeeInfo();
+    employee.displayEmployeeInfo();
+    employee.confirmInfo();
 
-int main(int argc, char *argv[]) {
-  QGuiApplication app(argc, argv);
-
-  TaskModel taskModel;
-  taskModel.addTask("Finish C++ assignment", QDateTime::currentDateTime().addDays(1), "CS-202", "todo", "");
-
-  QQmlApplicationEngine engine;
-  engine.rootContext()->setContextProperty("taskModel", &taskModel);
-
-  const QUrl url(u"qrc:/TaskGrid/Main.qml"_qs);
-  QObject::connect(&engine, &QQmlApplicationEngine::objectCreated, &app,
-    [url](QObject *obj, const QUrl &objUrl) {
-      if (!obj && url == objUrl) QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-  engine.load(url);
-
-  return app.exec();
+    return 0;
 }
