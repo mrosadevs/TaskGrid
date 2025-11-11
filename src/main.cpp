07e7f099b4fc13016de6fcb2ebf6816e70b78b74
mrosadevs <giftedlean@gmail.com>
2025-11-11 15:17:25 -0500

    Rebase

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,19 @@
-#include <iostream>
-#include "EmployeeInfo.h"
-using namespace std;
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QtCore/QString>          // ok to include; pulled by others anyway
+using namespace Qt::StringLiterals; // enables the _s literal
 
-int main()
-{
-    ConfirmInfo employee;
-    employee.inputEmployeeInfo();
-    employee.displayEmployeeInfo();
-    employee.confirmInfo();
+int main(int argc, char *argv[]) {
+    QGuiApplication app(argc, argv);
 
-    return 0;
+    QQmlApplicationEngine engine;
+    const QUrl url(u"qrc:/qt/qml/TaskGrid/Main.qml"_s);
+
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl) QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+
+    engine.load(url);
+    return app.exec();
 }
